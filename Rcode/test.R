f13d6d8c5f5bfa352f8e080cc155cff08992df52
Javier Elio <javiereliomedina@gmail.com>
2020-10-10 14:22:23 +0200

    add points

--- Rcode/test.R
+++ Rcode/test.R
@@ -1,9 +1,23 @@
 library(tidyverse)
 
-shots_data <- tibble(x = c(90, 85, 82, 78, 83),
-                         y = c(43, 40, 52, 56, 44))
+team_1 <- tribble(~ID, ~x, ~y, 
+                  1,  50, 45,
+                  2,  50, 44,
+                  3,  50, 43,
+                  4,  49, 44.5,
+                  5,  49, 43.5,
+                  6,  49, 42.5,
+                  7,  49, 45.5,
+                  8,  48, 44,
+                  9,  49, 47, 
+                  10, 40, 35,
+                  11, 20, 5,
+                  12, 35, 25,
+                  13, 30, 15, 
+                  14, 35, 60,
+                  15, 25, 50)
                                                  
-ggplot(shots_data, aes(x = x, y = y)) +
+ggplot() +
   annotate_pitch() +
   theme_pitch() +
-  geom_point() 
+  geom_point(data = team_1, aes(x = x, y = y), col = "red") 
